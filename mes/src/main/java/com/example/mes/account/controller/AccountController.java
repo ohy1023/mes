824a32aef1c102bf8b921145ec41815ef7884c31
ohy1023 <zvyg1023@naver.com>
2023-02-22 15:20:27 +0900

    factor : swagger 설명 추가

--- mes/src/main/java/com/example/mes/account/controller/AccountController.java
+++ mes/src/main/java/com/example/mes/account/controller/AccountController.java
@@ -4,6 +4,7 @@ import com.example.mes.account.dto.AccountDto;
 import com.example.mes.account.dto.AccountCreateRequest;
 import com.example.mes.common.dto.Response;
 import com.example.mes.account.service.AccountService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,12 +18,14 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @ApiOperation(value = "거래처 전체 조회")
     @GetMapping
     public ResponseEntity<Response<List<AccountDto>>> findAll() {
         List<AccountDto> accounts = accountService.findAllAccount();
         return ResponseEntity.ok().body(Response.success(accounts));
     }
 
+    @ApiOperation(value = "거래처 등록")
     @PostMapping
     public ResponseEntity<Response<AccountDto>> createAccount(@RequestBody AccountCreateRequest request) {
         AccountDto account = accountService.createAccount(request);
