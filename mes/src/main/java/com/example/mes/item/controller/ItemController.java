21da0e3be8f5e9b3988486743e9b85a345f8c8e1
ohy1023 <zvyg1023@naver.com>
2023-02-22 15:17:56 +0900

    factor : swagger 설명 추가

--- mes/src/main/java/com/example/mes/item/controller/ItemController.java
+++ mes/src/main/java/com/example/mes/item/controller/ItemController.java
@@ -8,6 +8,7 @@ import com.example.mes.item.dto.ItemDeleteResponse;
 import com.example.mes.item.dto.ItemUpdateResponse;
 import com.example.mes.common.dto.Response;
 import com.example.mes.item.service.ItemService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,12 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/v1/items")
 public class ItemController {
     private final ItemService itemService;
 
-    @PostMapping("/items/detail")
+    @ApiOperation(value = "품목 등록")
+    @PostMapping("/detail")
     public ResponseEntity<ItemDto> createItem(@RequestBody ItemCreateRequest itemCreateRequest) {
         log.info("companyName:{}", itemCreateRequest.getCompanyName());
         ItemDto itemDto = itemService.create(itemCreateRequest);
@@ -30,25 +32,30 @@ public class ItemController {
         return ResponseEntity.ok().body(itemDto);
     }
 
-    @GetMapping("/items")
+
+    @ApiOperation(value = "품목 전체 조회")
+    @GetMapping
     public ResponseEntity<List<ItemDto>> findAllItem() {
         List<ItemDto> itemDtoList = itemService.findAll();
         return ResponseEntity.ok().body(itemDtoList);
     }
 
-    @GetMapping("/items/company")
+    @ApiOperation(value = "회사 조회")
+    @GetMapping("/company")
     public ResponseEntity<List<String>> findCompanyList() {
         List<String> companyResponseList = itemService.findCompanyList();
         return ResponseEntity.ok().body(companyResponseList);
     }
 
-    @PostMapping("/items")
+    @ApiOperation(value = "품목 등록")
+    @PostMapping
     public ResponseEntity<ItemDto> addItem(@RequestBody ItemAddRequest request) {
         ItemDto itemDto = itemService.addItem(request);
         return ResponseEntity.ok().body(itemDto);
     }
 
-    @GetMapping("/items/search")
+    @ApiOperation(value = "품목 조건 검색")
+    @GetMapping("/search")
     public ResponseEntity<List<ItemDto>> findItemByCon(@RequestParam(required = false) String companyName, @RequestParam(required = false) String itemType) {
         log.info("companyName : {}",companyName);
         log.info("itemType : {}",itemType);
@@ -57,19 +64,22 @@ public class ItemController {
         return ResponseEntity.ok().body(itemDtoList);
     }
 
-    @GetMapping("/items/{itemId}")
+    @ApiOperation(value = "품목 상세 조회")
+    @GetMapping("/{itemId}")
     public ResponseEntity<Response<ItemDto>> findDetailItem(@PathVariable Long itemId) {
         ItemDto itemDto = itemService.findDetailItem(itemId);
         return ResponseEntity.ok().body(Response.success(itemDto));
     }
 
-    @PutMapping("/items/{itemId}")
+    @ApiOperation(value = "품목 수정")
+    @PutMapping("/{itemId}")
     public ResponseEntity<Response<ItemUpdateResponse>> updateItem(@PathVariable Long itemId, @RequestBody ItemUpdateRequest request) {
         ItemDto itemDto = itemService.updateItem(itemId, request);
         return ResponseEntity.ok().body(Response.success(new ItemUpdateResponse("품목 수정 완료", itemId)));
     }
 
-    @DeleteMapping("/items/{itemId}")
+    @ApiOperation(value = "품목 삭제")
+    @DeleteMapping("/{itemId}")
     public ResponseEntity<Response<ItemDeleteResponse>> deleteItem(@PathVariable Long itemId) {
         Long deleteItemId = itemService.deleteItem(itemId);
         return ResponseEntity.ok().body(Response.success(new ItemDeleteResponse("품목 삭제 완료", deleteItemId)));
