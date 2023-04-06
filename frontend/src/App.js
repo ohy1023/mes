db3ee842c3f0aa6c3d0de6838047172d996f51fe
ohy1023 <zvyg1023@naver.com>
2023-04-06 20:04:38 +0900

    feat : mypage 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,10 @@ import Footer from './components/common/footer/Footer';
 import Joinform from './components/pages/join/Joinform';
 import SignIn from './components/pages/login/SignIn';
 import MainPage from './components/pages/Mainpage/Mainpage';
+import Mypage from './components/pages/Mypage/Mypage';
+import PasswordValidation from './components/pages/Validation/PasswordValidation';
+import UserModifyForm from './components/pages/UserInfoModify/UserModifyForm';
+import PasswordModiftForm from './components/pages/UserInfoModify/PasswordModifyForm';
 
 
 function App() {
@@ -18,6 +22,10 @@ function App() {
               <Route path='/items' element={<Items/>}/>
               <Route path='/join' element={<Joinform/>}/>
               <Route path='/login' element={<SignIn/>}/>
+              <Route path='/mypage' element={<Mypage />} />
+              <Route path='/mypage/password/validation' element={<PasswordValidation />} />
+              <Route path='/mypage/info/modify' element={<UserModifyForm />} />
+              <Route path='/mypage/password/modify' element={<PasswordModiftForm />} />
           </Routes>
           <Footer/>
       </>
